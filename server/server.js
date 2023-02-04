ad337a9a6e4bb2ca54950d95c998afa0e0472f2c
Ilma Salsabil <106610322+crocmons@users.noreply.github.com>
2023-02-05 02:27:41 +0600

    express

--- server/server.js
+++ server/server.js
@@ -8,9 +8,8 @@ import Image from "./model/Image.js";
 
 dotenv.config();
 const app = express();
-app.use(express.json())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(cors());
 app.use(express.static("public"));
 
